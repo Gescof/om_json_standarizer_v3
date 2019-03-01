795a1e117cf6cfd77b03c93e519b7e7d98d59ddd
Yan <Yan@DESKTOP-MS487BF.home>
2019-03-01 18:18:54 +0100

    cambio a onesait

--- om_json_standarizer_v3/src/main/java/es/upm/etsisi/persistence/DAOMotaMeasures.java
+++ om_json_standarizer_v3/src/main/java/es/upm/etsisi/persistence/DAOMotaMeasures.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.minsait.onesait.platform.client.springboot.aspect.IoTBrokerRepository;
+
 import es.upm.etsisi.entities.mota.MotaMeasureTraza;
 
 /**
@@ -13,14 +15,16 @@ import es.upm.etsisi.entities.mota.MotaMeasureTraza;
  * @version 1.0
  */
 @Repository
-//@Sofia2Repository
+@IoTBrokerRepository("MotaMeasuresJ")
 public interface DAOMotaMeasures 
 extends MongoRepository<MotaMeasureTraza, String> {
 	
+	//@IoTBrokerQuery("select * from MotaMeasuresJ")
 	public List<MotaMeasureTraza> findAll();
 	
 	public Optional<MotaMeasureTraza> findByMotaMeasure_motaId(String id);
 	
+	//@IoTBrokerDelete
 	public void deleteByMotaMeasure_motaId(String id);
 	
 }
